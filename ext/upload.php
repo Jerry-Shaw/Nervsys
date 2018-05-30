363686f3c8e7f9bbab42e63ef44bb4f78d81d96f
Jerry <27206617@qq.com>
2018-05-30 13:06:29 +0800

    support multiple upload

--- ext/upload.php
+++ ext/upload.php
@@ -3,8 +3,7 @@
 /**
  * Upload Extension
  *
- * Copyright 2016-2017 Jerry Shaw <jerry-shaw@live.com>
- * Copyright 2017-2018 秋水之冰 <27206617@qq.com>
+ * Copyright 2016-2018 秋水之冰 <27206617@qq.com>
  * Copyright 2018 jresun <jresun@163.com>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -24,11 +23,14 @@ namespace ext;
 
 class upload
 {
+    //Upload file
+    public static $file = null;
+
+    //File name prefix
+    public static $prefix = '';
+
     //Allowed extensions
     public static $allowed = [];
-
-    //File name (without extension)
-    public static $file_name = '';
 
     //File permission
     public static $file_mode = 0664;
@@ -93,13 +95,65 @@ class upload
     ];
 
     /**
+     * Prepare upload files
+     *
+     * @param string $file
+     * @param string $type
+     *
+     * @return array
+     * @throws \Exception
+     */
+    private static function prep(string $file, string $type): array
+    {
+        $list = '' === $file ? (is_string(self::$file) ? [self::$file] : (is_array(self::$file) ? self::$file : [])) : [$file];
+
+        if ('file' === $type) {
+            foreach ($list as $key => $item) {
+                unset($list[$key]);
+
+                if (isset($_FILES[$item]) && isset($_FILES[$item]['tmp_name'])) {
+                    $list[$item] = $_FILES[$item];
+                }
+            }
+        } else {
+            foreach ($list as $key => $item) {
+                unset($list[$key]);
+
+                //Get base64 position
+                $pos = strpos($item, ';base64,');
+
+                //Base64 data error
+                if (false === $pos || 0 !== strpos($item, 'data:')) {
+                    continue;
+                }
+
+                $mime = (string)substr($item, 5, $pos - 5);
+
+                $list[$key]['ext'] = (string)array_search($mime, self::MIME, true);
+                $list[$key]['type'] = &$mime;
+                $list[$key]['data'] = base64_decode(substr($item, $pos + 8), true);
+
+                unset($pos, $mime);
+            }
+        }
+
+        if (empty($list)) {
+            throw new \Exception('NO uploaded files!');
+        }
+
+        unset($file, $type, $key, $item);
+        return $list;
+    }
+
+    /**
      * Upload file
      *
      * @param string $name
      *
      * @return array
+     * @throws \Exception
      */
-    public static function file(string $name): array
+    public static function file(string $name = ''): array
     {
         //Load language pack
         lang::$dir = 'upload';
@@ -109,62 +163,76 @@ class upload
         errno::$dir = 'upload';
         errno::load('ext', 'upload');
 
-        //File not exist
-        if (!isset($_FILES[$name]) || empty($_FILES[$name])) {
-            return errno::get(1007, 1);
+        //Get files
+        $files = self::prep($name, 'file');
+
+        //Reset files
+        self::$file = null;
+
+        //Upload
+        $result = [];
+        foreach ($files as $key => $file) {
+            //Server failed
+            if (0 !== $file['error']) {
+                $result[$key] = self::get_error($file['error']);
+                continue;
+            }
+
+            //Get file size
+            $file_size = self::chk_size($file['size']);
+
+            //File too large
+            if (0 === $file_size) {
+                $result[$key] = errno::get(1004, 1);
+                continue;
+            }
+
+            //Check file extension
+            $file_ext = self::chk_ext($file['name']);
+
+            //Extension not allowed
+            if ('' === $file_ext) {
+                $result[$key] = errno::get(1003, 1);
+                continue;
+            }
+
+            //Get upload path
+            $save_path = file::get_path(self::$path_save, self::$path_root, self::$path_mode);
+
+            //Upload path Error
+            if ('' === $save_path) {
+                $result[$key] = errno::get(1002, 1);
+                continue;
+            }
+
+            //Get file name
+            $save_name = '';
+
+            if ('' !== self::$prefix) {
+                $save_name .= self::$prefix . '_';
+            }
+
+            $save_name .= substr(hash('md5', uniqid(mt_rand(), true)), 16);
+
+            //Save file
+            $url = self::save_file($file['tmp_name'], $save_path, $save_name . '.' . $file_ext);
+
+            //Failed to move/copy
+            if ('' === $url) {
+                $result[$key] = errno::get(1001, 1);
+                continue;
+            }
+
+            //Upload done
+            $result[$key]['file_url'] = &$url;
+            $result[$key]['file_size'] = &$file_size;
+            $result[$key] += errno::get(1000, 0);
         }
 
-        //Copy file
-        $file = &$_FILES[$name];
-
-        //Upload failed when uploading, returned from server
-        if (0 !== $file['error']) {
-            return self::get_error($file['error']);
-        }
-
-        //Get file size
-        $file_size = self::chk_size($file['size']);
-
-        //File too large
-        if (0 === $file_size) {
-            return errno::get(1004, 1);
-        }
-
-        //Check file extension
-        $file_ext = self::chk_ext($file['name']);
-
-        //Extension not allowed
-        if ('' === $file_ext) {
-            return errno::get(1003, 1);
-        }
-
-        //Get upload path
-        $save_path = file::get_path(self::$path_save, self::$path_root, self::$path_mode);
-
-        //Upload path Error
-        if ('' === $save_path) {
-            return errno::get(1002, 1);
-        }
-
-        //Get file name
-        $save_name = '' !== self::$file_name ? self::$file_name : hash('md5', uniqid(mt_rand(), true));
-
-        //Save file
-        $url = self::save_file($file['tmp_name'], $save_path, $save_name . '.' . $file_ext);
-
-        //Failed to move/copy from tmp file
-        if ('' === $url) {
-            return errno::get(1001, 1);
-        }
-
-        //Upload done
-        $result = errno::get(1000, 0);
-        $result['file_url'] = &$url;
-        $result['file_size'] = &$file_size;
-
-        unset($name, $file, $file_size, $file_ext, $save_path, $save_name, $url);
+        unset($name, $files, $key, $file, $file_size, $file_ext, $save_path, $save_name, $url);
         return $result;
     }
+
 
     /**
      * Upload file via base64
@@ -184,19 +252,27 @@ class upload
         errno::$dir = 'upload';
         errno::load('ext', 'upload');
 
-        //Get base64 position
-        $data_pos = strpos($base64, ';base64,');
+        //Get files
+        $files = self::prep($base64, 'base64');
 
-        //MIME-Type not allowed
-        if (false === $data_pos || 0 !== strpos($base64, 'data:')) {
-            return errno::get(1003, 1);
+        //Reset files
+        self::$file = null;
+
+        //Upload
+        $result = [];
+        foreach ($files as $key => $file) {
+
+
+            
+
+
         }
 
-        //Get MIME-Type
-        $mime_type = (string)substr($base64, 5, $data_pos - 5);
 
-        //Get extension from allowed MIME-Type list
-        $file_ext = (string)array_search($mime_type, self::MIME, true);
+
+
+
+
 
         //Check file extension
         $file_ext = self::chk_ext($file_ext);
