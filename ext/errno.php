b53189988c586b6dcfd9fab0bb2385076e832246
Jerry <27206617@qq.com>
2018-07-28 14:56:37 +0800

    finish error extension

---
+++ ext/errno.php
@@ -0,0 +1,88 @@
+<?php
+
+/**
+ * Errno Extension
+ *
+ * Copyright 2016-2018 秋水之冰 <27206617@qq.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace ext;
+
+use core\system;
+
+class errno extends system
+{
+    /**
+     * Error file directory
+     * Related to "ROOT/$dir/"
+     * Error file should be located in "ROOT/$dir/$this->dir/filename.ini"
+     *
+     * @var string
+     */
+    public $dir = DIRECTORY_SEPARATOR . 'error' . DIRECTORY_SEPARATOR;
+
+    //Multi-language support
+    public $lang = true;
+
+    //Error message pool
+    private $pool = [];
+
+    /**
+     * Load error file
+     *
+     * @param string $dir
+     * @param string $name
+     *
+     * @throws \ErrorException
+     */
+    public function __construct(string $dir, string $name)
+    {
+        $data = parse_ini_file(ROOT . $dir . $this->dir . $name . '.ini', false);
+
+        if (false === $data) {
+            throw new \ErrorException('Failed to read [' . $name . '.ini]!');
+        }
+
+        $this->pool += $data;
+        unset($dir, $name, $data);
+    }
+
+    /**
+     * Set standard output error
+     *
+     * @param int $code
+     * @param int $errno
+     */
+    public function set(int $code, int $errno = 0): void
+    {
+        parent::$error = $this->get($code, $errno);
+    }
+
+    /**
+     * Get standard error result
+     * Language pack should be loaded before getting an error message on multi-language support system
+     *
+     * @param int $code
+     * @param int $errno
+     *
+     * @return array
+     */
+    public function get(int $code, int $errno = 0): array
+    {
+        return isset($this->pool[$code])
+            ? ['err' => &$errno, 'code' => &$code, 'msg' => $this->lang ? gettext($this->pool[$code]) : $this->pool[$code]]
+            : ['err' => &$errno, 'code' => &$code, 'msg' => 'Error message NOT found!'];
+    }
+}
