02435eec3dffb3f1ff3af25c4eeb6281b96b0e36
Jerry <27206617@qq.com>
2019-01-17 11:13:00 +0800

    update copyright year

--- ext/memcached.php
+++ ext/memcached.php
@@ -3,7 +3,7 @@
 /**
  * Memcached Connector Extension
  *
- * Copyright 2018 tggtzbh <tggtzbh@sina.com>
+ * Copyright 2018-2019 tggtzbh <tggtzbh@sina.com>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
