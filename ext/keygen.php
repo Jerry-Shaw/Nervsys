f5cce856fbb4063ac196eb64b899082406607132
Jerry <27206617@qq.com>
2019-03-15 22:00:30 +0800

    Optimize default keygen extension (formula changed, not compatible)

--- ext/keygen.php
+++ ext/keygen.php
@@ -43,10 +43,10 @@ class keygen implements key
      */
     public static function extract(string $key): array
     {
-        $keys        = [];
-        $keys['key'] = &$key;
+        $keys = [];
 
-        $keys['iv'] = 0 === ord(substr($key, 0, 1)) & 1
+        $keys['key'] = &$key;
+        $keys['iv']  = 0 === (ord(substr($key, 0, 1)) & 1)
             ? substr($key, 0, 16)
             : substr($key, -16, 16);
 
@@ -68,7 +68,7 @@ class keygen implements key
         foreach ($unit as $k => $v) {
             $unit_key = substr($v, 0, 1);
 
-            if ($k & 1 !== ord($unit_key) & 1) {
+            if (self::valid_kv($k, $unit_key)) {
                 $v = strrev($v);
             }
 
@@ -95,12 +95,26 @@ class keygen implements key
         foreach ($unit as $k => $v) {
             $unit_key  = substr($v, -1, 1);
             $unit_item = substr($v, 0, 4);
-            $unit[$k]  = ($k & 1 !== ord($unit_key) & 1) ? strrev($unit_item) : $unit_item;
+
+            $unit[$k] = self::valid_kv($k, $unit_key) ? strrev($unit_item) : $unit_item;
         }
 
         $key = implode($unit);
 
         unset($unit, $k, $v, $unit_key, $unit_item);
         return $key;
+    }
+
+    /**
+     * Valid k & v
+     *
+     * @param int    $k
+     * @param string $v
+     *
+     * @return bool
+     */
+    private static function valid_kv(int $k, string $v): bool
+    {
+        return 0 === ($k & ord($v));
     }
 }
