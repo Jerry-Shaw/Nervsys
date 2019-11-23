55c0f3653c6aeba20b52b47d3655b2731e6b00ed
Jerry <27206617@qq.com>
2019-11-23 12:15:58 +0800

    using str[0] instead of substr(str, 0, 1) in some cases

--- ext/crypt.php
+++ ext/crypt.php
@@ -206,7 +206,7 @@ class crypt extends factory
 
         $noises = str_split($key, 8);
 
-        $string = 0 === (ord(substr($key, 0, 1)) & 1)
+        $string = 0 === (ord($key[0]) & 1)
             ? $noises[0] . ':' . $string . ':' . $noises[2]
             : $noises[1] . ':' . $string . ':' . $noises[3];
 
