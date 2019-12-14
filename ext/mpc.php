3118acfac0a6e476ba9c6472639bd6411758c31b
Jerry <27206617@qq.com>
2019-12-14 10:51:32 +0800

    minor change on log file name

--- ext/mpc.php
+++ ext/mpc.php
@@ -186,7 +186,7 @@ class mpc extends factory
                 [
                     ['pipe', 'r'],
                     ['pipe', 'w'],
-                    ['file', \core\lib\stc\factory::build(pool::class)->conf['log']['save_path'] . DIRECTORY_SEPARATOR . date('Y-m-d') . '_mpc_error' . '.log', 'ab+']
+                    ['file', \core\lib\stc\factory::build(pool::class)->conf['log']['save_path'] . DIRECTORY_SEPARATOR . date('Ymd') . '_mpc_error' . '.log', 'ab+']
                 ],
                 $pipes
             );
