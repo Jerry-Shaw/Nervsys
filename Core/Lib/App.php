277ab2aa2b3b9859d87eda622e6a993b55bfabf0
Jerry <27206617@qq.com>
2020-08-07 16:41:16 +0800

    App library done

---
+++ Core/Lib/App.php
@@ -0,0 +1,171 @@
+<?php
+
+/**
+ * NS App library
+ *
+ * Copyright 2016-2020 Jerry Shaw <jerry-shaw@live.com>
+ * Copyright 2016-2020 秋水之冰 <27206617@qq.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Core\Lib;
+
+use Core\Factory;
+
+/**
+ * Class App
+ *
+ * @package Core\Lib
+ */
+class App extends Factory
+{
+    public string $root_path;
+    public string $entry_path;
+    public string $script_path;
+
+    public string $api_path = 'api';
+    public string $app_path = 'app';
+    public string $inc_path = 'inc';
+
+    public string $timezone = 'Asia/Shanghai';
+
+    public bool $auto_call  = false;
+    public bool $core_debug = false;
+
+    /**
+     * App constructor.
+     */
+    public function __construct()
+    {
+        //Get script file path
+        $this->script_path = strtr($_SERVER['SCRIPT_FILENAME'], '\\/', DIRECTORY_SEPARATOR . DIRECTORY_SEPARATOR);
+
+        //Get absolute path of entry script
+        if (DIRECTORY_SEPARATOR !== $this->script_path[0] && ':' !== $this->script_path[1]) {
+            $this->script_path = getcwd() . DIRECTORY_SEPARATOR . $this->script_path;
+        }
+
+        //Get absolute entry path
+        $this->entry_path = dirname($this->script_path);
+
+        //Get absolute root path
+        $this->root_path = $this->getPath($this->app_path);
+    }
+
+    /**
+     * Set api pathname
+     *
+     * @param string $pathname
+     *
+     * @return $this
+     */
+    public function setApiPath(string $pathname): self
+    {
+        $this->api_path = &$pathname;
+
+        unset($pathname);
+        return $this;
+    }
+
+    /**
+     * Set app pathname and root path
+     *
+     * @param string $pathname
+     *
+     * @return $this
+     */
+    public function setAppPath(string $pathname): self
+    {
+        $this->app_path  = &$pathname;
+        $this->root_path = $this->getPath($pathname);
+
+        unset($pathname);
+        return $this;
+    }
+
+    /**
+     * Set inc pathname
+     *
+     * @param string $pathname
+     *
+     * @return $this
+     */
+    public function setIncPath(string $pathname): self
+    {
+        $this->inc_path = &$pathname;
+
+        unset($pathname);
+        return $this;
+    }
+
+    /**
+     * Set default timezone
+     *
+     * @param string $timezone
+     *
+     * @return $this
+     */
+    public function setTimezone(string $timezone): self
+    {
+        $this->timezone = &$timezone;
+
+        unset($timezone);
+        return $this;
+    }
+
+    /**
+     * Set auto_call mode
+     *
+     * @param bool $auto_call_mode
+     *
+     * @return $this
+     */
+    public function setAutoCall(bool $auto_call_mode): self
+    {
+        $this->auto_call = &$auto_call_mode;
+
+        unset($auto_call_mode);
+        return $this;
+    }
+
+    /**
+     * Set core_debug mode
+     *
+     * @param bool $core_debug_mode
+     *
+     * @return $this
+     */
+    public function setCoreDebug(bool $core_debug_mode): self
+    {
+        $this->core_debug = &$core_debug_mode;
+
+        unset($core_debug_mode);
+        return $this;
+    }
+
+    /**
+     * Get target path based on app path
+     *
+     * @param string $pathname
+     *
+     * @return string
+     */
+    private function getPath(string $pathname): string
+    {
+        $target_path = is_dir(($parent_path = dirname($this->entry_path)) . DIRECTORY_SEPARATOR . $pathname) ? $parent_path : $this->entry_path;
+
+        unset($pathname, $parent_path);
+        return $target_path;
+    }
+}
