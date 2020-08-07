0c4ed726c44a5538b129f32415e204463265b761
Jerry <27206617@qq.com>
2020-08-07 16:40:45 +0800

    Start new version of NS

---
+++ NS.php
@@ -0,0 +1,111 @@
+<?php
+
+/**
+ * NS System script
+ *
+ * Copyright 2016-2020 Jerry Shaw <jerry-shaw@live.com>
+ * Copyright 2016-2020 秋水之冰 <27206617@qq.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+//Strict type declare
+declare(strict_types = 1);
+
+//Require PHP version >= 7.4.0
+if (version_compare(PHP_VERSION, '7.4.0', '<')) {
+    exit('NervSys needs PHP 7.4.0 or higher!');
+}
+
+//Define NervSys version
+define('NS_VER', '8.0.0 Alpha');
+
+//Define SYSTEM ROOT path
+define('NS_ROOT', __DIR__);
+
+//Define JSON formats
+define('JSON_FORMAT', JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_LINE_TERMINATORS);
+define('JSON_PRETTY', JSON_FORMAT | JSON_PRETTY_PRINT);
+
+//Detect extension support
+define('SPT_OPC', extension_loaded('Zend OPcache'));
+define('SPT_SHM', extension_loaded('shmop'));
+
+//Autoload function
+function Autoload(string $class_name, string $root_path = NS_ROOT): void
+{
+    //Get relative path of class file
+    $file_name = strtr($class_name, '\\', DIRECTORY_SEPARATOR) . '.php';
+
+    //Skip non-existent class file
+    if (!is_file($class_file = $root_path . DIRECTORY_SEPARATOR . $file_name)) {
+        return;
+    }
+
+    //Compile/require class file
+    $file_compiled = false;
+
+    if (SPT_OPC && 0 === strpos($class_file, NS_ROOT)) {
+        $file_compiled = opcache_compile_file($class_file);
+    }
+
+    if (!$file_compiled) {
+        require $class_file;
+    }
+
+    unset($class_name, $root_path, $file_name, $class_file, $file_compiled);
+}
+
+//Compile Factory module
+Autoload(\Core\Factory::class);
+
+//Register autoload (ROOT_SYS based)
+spl_autoload_register(
+    static function (string $class_name): void
+    {
+        Autoload($class_name);
+    }
+);
+
+/**
+ * Class NS
+ */
+class NS
+{
+    /**
+     * NS constructor.
+     */
+    public function __construct()
+    {
+        //Misc settings
+        set_time_limit(0);
+        ignore_user_abort(true);
+
+        //Set error_reporting level
+        error_reporting(E_ALL);
+
+        //Init App
+        $App = \Core\Lib\App::new();
+
+        //Set include path
+        set_include_path($App->root_path . DIRECTORY_SEPARATOR . $App->inc_path);
+
+        //Set default timezone
+        date_default_timezone_set($App->timezone);
+
+
+
+
+
+    }
+}
