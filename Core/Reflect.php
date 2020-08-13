22e3f38e98d89da9c1d1496c3420656107997b70
Jerry <27206617@qq.com>
2020-08-13 22:53:47 +0800

    Finish Reflect class

--- Core/Reflect.php
+++ Core/Reflect.php
@@ -21,8 +21,226 @@
 
 namespace Core;
 
-
-class Reflect
+/**
+ * Class Reflect
+ *
+ * @package Core
+ */
+class Reflect extends Factory
 {
+    private array $pool = [];
 
+    /**
+     * Get class reflection
+     *
+     * @param string $class
+     *
+     * @return \ReflectionClass
+     * @throws \ReflectionException
+     */
+    public function getClass(string $class): \ReflectionClass
+    {
+        if (!isset($this->pool[$class])) {
+            $this->pool[$class] = new \ReflectionClass($class);
+        }
+
+        return $this->pool[$class];
+    }
+
+    /**
+     * Get method reflection
+     *
+     * @param string $class
+     * @param string $method
+     *
+     * @return \ReflectionMethod
+     * @throws \ReflectionException
+     */
+    public function getMethod(string $class, string $method): \ReflectionMethod
+    {
+        if (!isset($this->pool[$key = $class . ':' . $method])) {
+            $this->pool[$key] = $this->getClass($class)->getMethod($method);
+        }
+
+        unset($class, $method);
+        return $this->pool[$key];
+    }
+
+    /**
+     * Get all params from method
+     *
+     * @param string $class
+     * @param string $method
+     *
+     * @return array
+     * @throws \ReflectionException
+     */
+    public function getParams(string $class, string $method): array
+    {
+        if (!isset($this->pool[$key = $class . '>' . $method])) {
+            $this->pool[$key] = $this->getMethod($class, $method)->getParameters();
+        }
+
+        unset($class, $method);
+        return $this->pool[$key];
+    }
+
+    /**
+     * Get method list from class
+     *
+     * @param string $class
+     * @param int    $filter
+     *
+     * @return array
+     * @throws \ReflectionException
+     */
+    public function getMethods(string $class, int $filter): array
+    {
+        return $this->getClass($class)->getMethods($filter);
+    }
+
+    /**
+     * Get return type hint
+     *
+     * @param string $class
+     * @param string $method
+     *
+     * @return string
+     * @throws \ReflectionException
+     */
+    public function getReturnType(string $class, string $method): string
+    {
+        $return_type = !is_null($type_reflect = $this->getMethod($class, $method)->getReturnType()) ? $type_reflect->getName() : '';
+
+        unset($class, $method, $type_reflect);
+        return $return_type;
+    }
+
+    /**
+     * Get information of a param
+     *
+     * @param \ReflectionParameter $parameter
+     *
+     * @return array
+     * @throws \ReflectionException
+     */
+    public function getParamInfo(\ReflectionParameter $parameter): array
+    {
+        $info = [];
+
+        //Get name
+        $info['name'] = $parameter->getName();
+
+        //Get default value
+        $info['has_default'] = $parameter->isDefaultValueAvailable();
+        if ($info['has_default']) {
+            $info['default'] = $parameter->getDefaultValue();
+        }
+
+        //Get param type
+        $info['has_type'] = $parameter->hasType();
+        if ($info['has_type']) {
+            $info['type'] = $parameter->getType()->getName();
+
+            //Get class name
+            $param_class       = $parameter->getClass();
+            $info['has_class'] = is_object($param_class);
+
+            if ($info['has_class']) {
+                $info['class'] = $param_class->getName();
+            }
+
+            unset($param_class);
+        } else {
+            $info['has_class'] = false;
+        }
+
+        unset($parameter);
+        return $info;
+    }
+
+    /**
+     * Build params for a method
+     *
+     * @param string $class
+     * @param string $method
+     * @param array  $inputs
+     *
+     * @return array
+     * @throws \ReflectionException
+     */
+    public function buildParams(string $class, string $method, array $inputs): array
+    {
+        //Default result
+        $result = ['param' => [], 'diff' => []];
+
+        //Get needed params
+        $need_params = $this->getParams($class, $method);
+
+        /** @var \ReflectionParameter $param_reflect */
+        foreach ($need_params as $param_reflect) {
+            $param_info = $this->getParamInfo($param_reflect);
+
+            //Dependency injection
+            if ($param_info['has_class']) {
+                $result['param'][] = parent::getObj($param_info['class']);
+                continue;
+            }
+
+            //Param NOT exists
+            if (!isset($inputs[$param_info['name']])) {
+                //Check default value
+                $param_info['has_default']
+                    ? $result['param'][] = $param_info['default']
+                    : $result['diff'][] = $param_info['name'];
+                continue;
+            }
+
+            //Param without type
+            if (!$param_info['has_type']) {
+                $result['param'][] = $inputs[$param_info['name']];
+                continue;
+            }
+
+            //Type detection
+            switch ($param_info['type']) {
+                case 'int':
+                    is_numeric($inputs[$param_info['name']])
+                        ? $result['param'][] = (int)$inputs[$param_info['name']]
+                        : $result['diff'][] = $param_info['name'];
+                    break;
+                case 'bool':
+                    is_bool($inputs[$param_info['name']])
+                        ? $result['param'][] = (bool)$inputs[$param_info['name']]
+                        : $result['diff'][] = $param_info['name'];
+                    break;
+                case 'float':
+                    is_numeric($inputs[$param_info['name']])
+                        ? $result['param'][] = (float)$inputs[$param_info['name']]
+                        : $result['diff'][] = $param_info['name'];
+                    break;
+                case 'array':
+                    is_array($inputs[$param_info['name']]) || is_object($inputs[$param_info['name']])
+                        ? $result['param'][] = (array)$inputs[$param_info['name']]
+                        : $result['diff'][] = $param_info['name'];
+                    break;
+                case 'string':
+                    is_string($inputs[$param_info['name']]) || is_numeric($inputs[$param_info['name']])
+                        ? $result['param'][] = trim((string)$inputs[$param_info['name']])
+                        : $result['diff'][] = $param_info['name'];
+                    break;
+                case 'object':
+                    is_object($inputs[$param_info['name']]) || is_array($inputs[$param_info['name']])
+                        ? $result['param'][] = (object)$inputs[$param_info['name']]
+                        : $result['diff'][] = $param_info['name'];
+                    break;
+                default:
+                    $result['param'][] = $inputs[$param_info['name']];
+                    break;
+            }
+        }
+
+        unset($class, $method, $inputs, $need_params, $param_reflect, $param_info);
+        return $result;
+    }
 }
