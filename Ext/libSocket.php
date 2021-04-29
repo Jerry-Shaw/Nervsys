ba47644a74e175347ec61cbe0b589250b5c09e43
Jerry <27206617@qq.com>
2021-04-29 15:59:45 +0800

    Add ws_* options to readMsg and sendMsg to provide encode/decode functions internally

--- Ext/libSocket.php
+++ Ext/libSocket.php
@@ -281,10 +281,11 @@ class libSocket extends Factory
      * Read message from client
      *
      * @param string $sock_id
+     * @param bool   $ws_decode
      *
      * @return array
      */
-    public function readMsg(string $sock_id): array
+    public function readMsg(string $sock_id, bool $ws_decode = false): array
     {
         try {
             if (false === ($msg = fread($this->socket_clients[$sock_id], 1024))) {
@@ -295,6 +296,10 @@ class libSocket extends Factory
                 $msg .= $buff;
             }
 
+            if ($ws_decode) {
+                $msg = $this->wsDecode($msg);
+            }
+
             $this->socket_actives[$sock_id] = time();
             $this->showLog('read', $sock_id . ': ' . $msg);
 
@@ -303,11 +308,11 @@ class libSocket extends Factory
             $this->showLog('exit', $throwable->getMessage());
             $this->close($sock_id);
 
-            unset($throwable, $sock_id, $msg);
+            unset($throwable, $sock_id, $ws_decode, $msg);
             $result = ['len' => -1, 'msg' => ''];
         }
 
-        unset($sock_id, $msg, $buff);
+        unset($sock_id, $ws_decode, $msg, $buff);
         return $result;
     }
 
@@ -316,23 +321,24 @@ class libSocket extends Factory
      *
      * @param string $sock_id
      * @param string $message
+     * @param bool   $ws_encode
      *
      * @return int
      */
-    public function sendMsg(string $sock_id, string $message): int
+    public function sendMsg(string $sock_id, string $message, bool $ws_encode = false): int
     {
         try {
-            $byte = fwrite($this->socket_clients[$sock_id], $message);
+            $byte = fwrite($this->socket_clients[$sock_id], $ws_encode ? $this->wsEncode($message) : $message);
             $this->showLog('send', $sock_id . ': ' . $message);
         } catch (\Throwable $throwable) {
             $this->showLog('exit', '"' . $sock_id . '" send ERROR!');
             $this->close($sock_id);
 
-            unset($throwable, $sock_id, $message, $byte);
+            unset($throwable, $sock_id, $message, $ws_encode, $byte);
             return -1;
         }
 
-        unset($sock_id, $message);
+        unset($sock_id, $message, $ws_encode);
         return $byte;
     }
 
