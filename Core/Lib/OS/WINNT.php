a60743fd049a008e7ffb9bb5cfc874d4f2f101a6
Jerry <27206617@qq.com>
2022-01-21 15:35:26 +0800

    WINNT improved

--- Core/Lib/OS/WINNT.php
+++ Core/Lib/OS/WINNT.php
@@ -5,7 +5,7 @@
  *
  * Copyright 2016-2021 Jerry Shaw <jerry-shaw@live.com>
  * Copyright 2016-2021 秋水之冰 <27206617@qq.com>
-  * Copyright 2016-2021 take your time <704505144@qq.com>
+ * Copyright 2016-2021 take your time <704505144@qq.com>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -39,32 +39,23 @@ class WINNT
      */
     public function getHwHash(): string
     {
-        $queries = [
-            'wmic cpu get Caption, CreationClassName, Family, Manufacturer, Name, ProcessorId, ProcessorType, Revision /format:value',
-            'wmic baseboard get Manufacturer, Product, SerialNumber, Version /format:value',
-            'wmic nic where netconnectionid!=NULL get macaddress /format:value',
-            'wmic memorychip get BankLabel, Capacity /format:value',
-            'wmic bios get SerialNumber /format:value'
-        ];
-        $cmd = "powershell -Command \"";
-	$queries  = [
-		"Get-WmiObject -class Win32_Processor | select Caption, CreationClassName, Family, Manufacturer, Name, ProcessorId, ProcessorType, Revision",
-		"Get-WmiObject -class Win32_BaseBoard  | select Manufacturer, Product, SerialNumber, Version",
-		"Get-WmiObject -class Win32_NetworkAdapter  -Filter \"netconnectionid!=NULL\" | select macaddress",
-		"Get-WmiObject -class Win32_PhysicalMemory   | select BankLabel, Capacity",
-		"Get-WmiObject -class Win32_BIOS   | select SerialNumber",			
-		];	
-	$cmd .= implode(';',$queries) . "\"";
-        exec($cmd, $output, $status);
+        $ps_cmd = 'powershell -Command "';
+        $ps_cmd .= 'Get-CimInstance -class Win32_Processor | select Caption, CreationClassName, Family, Manufacturer, Name, ProcessorId, ProcessorType;';
+        $ps_cmd .= 'Get-CimInstance -class Win32_NetworkAdapter -Filter "netconnectionid!=NULL" | select macaddress;';
+        $ps_cmd .= 'Get-CimInstance -class Win32_BaseBoard | select Manufacturer, Product, SerialNumber, Version;';
+        $ps_cmd .= 'Get-CimInstance -class Win32_PhysicalMemory | select Capacity;';
+        $ps_cmd .= 'Get-CimInstance -class Win32_BIOS | select SerialNumber';
+        $ps_cmd .= '"';
+
+        exec($ps_cmd, $output, $status);
+
         if (0 !== $status) {
             throw new \Exception(PHP_OS . ': Access denied!', E_USER_ERROR);
         }
 
-        $output = array_filter($output);
-        $output = array_unique($output);
-        $hw_hash = hash('md5', json_encode($output));
+        $hw_hash = hash('md5', json_encode(array_filter($output)));
 
-        unset($queries, $output, $query, $status);
+        unset($ps_cmd, $output, $status);
         return $hw_hash;
     }
 
@@ -76,21 +67,27 @@ class WINNT
      */
     public function getPhpPath(): string
     {
-     $str = "powershell -Command \"Get-WmiObject -class Win32_process -Filter \"ProcessId=".getmypid()."\"  |  select ExecutablePath\"";
-        exec($str,$output,$status);
-	$output = array_filter($output);
-	$output = array_unique($output);
+        $ps_cmd = 'powershell -Command "Get-CimInstance -class Win32_process -Filter "ProcessId=' . getmypid() . '" | select ExecutablePath | Format-List"';
+
+        exec($ps_cmd, $output, $status);
+
         if (0 !== $status) {
             throw new \Exception(PHP_OS . ': Access denied!', E_USER_ERROR);
         }
 
-        if (!isset($output[3]) || '' === $output[3]) {
+        $process_info = current(array_filter($output));
+
+        if (false === $process_info || false === ($mark_pos = strpos($process_info, ':'))) {
             throw new \Exception(PHP_OS . ': PHP path NOT found!', E_USER_ERROR);
         }
 
-        $php_path = &$output[3];
+        $php_path = trim(substr($process_info, $mark_pos + 2));
 
-        unset($output, $status);
+        if (!is_file($php_path)) {
+            throw new \Exception(PHP_OS . ': PHP path ERROR!', E_USER_ERROR);
+        }
+
+        unset($ps_cmd, $output, $status, $process_info, $mark_pos);
         return $php_path;
     }
 
@@ -101,7 +98,7 @@ class WINNT
      */
     public function setAsBg(): self
     {
-        $this->os_cmd = 'start "" /B ' . $this->os_cmd . ' >nul 2>&1';
+        $this->os_cmd = 'start "" / B ' . $this->os_cmd . ' > nul 2 >&1';
         return $this;
     }
 
