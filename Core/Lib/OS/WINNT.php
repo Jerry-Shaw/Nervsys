04dd67270ca184ecedf1411f7d656747e9363f15
core <704505144@qq.com>
2022-01-21 12:56:49 +0800

    winnt (#72)

--- Core/Lib/OS/WINNT.php
+++ Core/Lib/OS/WINNT.php
@@ -5,6 +5,7 @@
  *
  * Copyright 2016-2021 Jerry Shaw <jerry-shaw@live.com>
  * Copyright 2016-2021 秋水之冰 <27206617@qq.com>
+  * Copyright 2016-2021 take your time <704505144@qq.com>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -45,16 +46,22 @@ class WINNT
             'wmic memorychip get BankLabel, Capacity /format:value',
             'wmic bios get SerialNumber /format:value'
         ];
-
-        exec(implode(' && ', $queries), $output, $status);
-
+        $cmd = "powershell -Command \"";
+	$queries  = [
+		"Get-WmiObject -class Win32_Processor | select Caption, CreationClassName, Family, Manufacturer, Name, ProcessorId, ProcessorType, Revision",
+		"Get-WmiObject -class Win32_BaseBoard  | select Manufacturer, Product, SerialNumber, Version",
+		"Get-WmiObject -class Win32_NetworkAdapter  -Filter \"netconnectionid!=NULL\" | select macaddress",
+		"Get-WmiObject -class Win32_PhysicalMemory   | select BankLabel, Capacity",
+		"Get-WmiObject -class Win32_BIOS   | select SerialNumber",			
+		];	
+	$cmd .= implode(';',$queries) . "\"";
+        exec($cmd, $output, $status);
         if (0 !== $status) {
             throw new \Exception(PHP_OS . ': Access denied!', E_USER_ERROR);
         }
 
         $output = array_filter($output);
         $output = array_unique($output);
-
         $hw_hash = hash('md5', json_encode($output));
 
         unset($queries, $output, $query, $status);
@@ -69,17 +76,19 @@ class WINNT
      */
     public function getPhpPath(): string
     {
-        exec('wmic process where ProcessId="' . getmypid() . '" get ExecutablePath', $output, $status);
-
+     $str = "powershell -Command \"Get-WmiObject -class Win32_process -Filter \"ProcessId=".getmypid()."\"  |  select ExecutablePath\"";
+        exec($str,$output,$status);
+	$output = array_filter($output);
+	$output = array_unique($output);
         if (0 !== $status) {
             throw new \Exception(PHP_OS . ': Access denied!', E_USER_ERROR);
         }
 
-        if (!isset($output[1]) || '' === $output[1]) {
+        if (!isset($output[3]) || '' === $output[3]) {
             throw new \Exception(PHP_OS . ': PHP path NOT found!', E_USER_ERROR);
         }
 
-        $php_path = &$output[1];
+        $php_path = &$output[3];
 
         unset($output, $status);
         return $php_path;
