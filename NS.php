6bec1c3f810646557a02cf21c502f0165b08d016
Jerry <27206617@qq.com>
2022-02-10 11:19:14 +0800

    Keep all in class

--- NS.php
+++ NS.php
@@ -1,10 +1,10 @@
 <?php
 
 /**
- * NS System script
+ * Nervsys main script
  *
- * Copyright 2016-2021 Jerry Shaw <jerry-shaw@live.com>
- * Copyright 2016-2021 秋水之冰 <27206617@qq.com>
+ * Copyright 2016-2022 Jerry Shaw <jerry-shaw@live.com>
+ * Copyright 2016-2022 秋水之冰 <27206617@qq.com>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,110 +19,42 @@
  * limitations under the License.
  */
 
-declare(strict_types = 1);
+namespace Nervsys;
 
-use Core\Execute;
-use Core\Factory;
-use Core\Lib\App;
-use Core\Lib\CORS;
-use Core\Lib\Error;
-use Core\Lib\IOUnit;
-use Core\Lib\Router;
-
-//Misc settings
-set_time_limit(0);
-ignore_user_abort(true);
-
-//Require PHP version >= 7.4.0
-if (version_compare(PHP_VERSION, '7.4.0', '<')) {
-    exit('NervSys needs PHP 7.4.0 or higher!');
-}
-
-//Define NervSys version
-const NS_VER = '8.0.3';
-
-//Define SYSTEM ROOT path
-const NS_ROOT = __DIR__;
-
-//Define JSON formats
-const JSON_FORMAT = JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_LINE_TERMINATORS;
-const JSON_PRETTY = JSON_FORMAT | JSON_PRETTY_PRINT;
-
-//Autoload function
-function autoload(string $class_name, string $root_path = NS_ROOT): void
-{
-    //Get relative path of class file
-    $file_name = strtr($class_name, '\\', DIRECTORY_SEPARATOR) . '.php';
-
-    if (false === strpos($class_name, '\\')) {
-        //Load script file from include path
-        require $file_name;
-    } elseif (is_file($class_file = $root_path . DIRECTORY_SEPARATOR . $file_name)) {
-        //Require class file
-        require $class_file;
-    }
-
-    unset($class_name, $root_path, $file_name, $class_file);
-}
-
-//Register autoload (NS_ROOT based)
-spl_autoload_register(
-    static function (string $class_name): void
-    {
-        autoload($class_name);
-        unset($class_name);
-    }
-);
-
-//Init App library
-App::new();
-
-/**
- * Class NS
- */
-class NS extends Factory
+class NS
 {
     /**
-     * NS constructor.
+     * NS constructor
      */
     public function __construct()
     {
-        //Init Error library
-        $error = Error::new();
+        set_time_limit(0);
+        ignore_user_abort(true);
 
-        //Register error handler
-        register_shutdown_function($error->shutdown_handler);
-        set_exception_handler($error->exception_handler);
-        set_error_handler($error->error_handler);
+        define('NS_VER', '8.1.0');
+        define('NS_ROOT', __DIR__);
 
-        //Init App library
-        $app = App::new();
+        define('JSON_FORMAT', JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_LINE_TERMINATORS);
+        define('JSON_PRETTY', JSON_FORMAT | JSON_PRETTY_PRINT);
 
-        //Set default timezone
-        date_default_timezone_set($app->timezone);
+        spl_autoload_register(
+            function (string $class): void
+            {
+                $preloaded = false;
+                $file_path = __DIR__ . strtr(strstr($class, '\\'), '\\', DIRECTORY_SEPARATOR) . '.php';
 
-        //Check CORS Permission
-        CORS::new()->checkPerm($app->is_cli, $app->is_tls);
+                if (function_exists('opcache_compile_file')) {
+                    $preloaded = opcache_compile_file($file_path);
+                }
 
-        //Init IOUnit library
-        $IOUnit = IOUnit::new();
+                if (!$preloaded && is_file($file_path)) {
+                    require $file_path;
+                }
 
-        //Read input data
-        !$app->is_cli ? $IOUnit->readCgi() : $IOUnit->readCli();
-
-        if ('' !== ($IOUnit->src_cmd = trim($IOUnit->src_cmd))) {
-            //Init Router library
-            $router = Router::new()->parse($IOUnit->src_cmd);
-
-            //Init Execute Module
-            $execute = Execute::new()->copyCmd($router);
-
-            //Execute process & fetch results
-            $IOUnit->src_output += $execute->callCli();
-            $IOUnit->src_output += $execute->callCgi();
-        }
-
-        //Output results
-        $IOUnit->output();
+                unset($class, $preloaded, $file_path);
+            },
+            true,
+            true
+        );
     }
 }
