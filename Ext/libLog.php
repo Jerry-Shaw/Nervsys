ad785656fb1d8de56e7f3ad5856fde99b0641cb4
Jerry <27206617@qq.com>
2022-02-22 10:38:16 +0800

    Add libLog

---
+++ Ext/libLog.php
@@ -0,0 +1,86 @@
+<?php
+
+/**
+ * Log Extension
+ *
+ * Copyright 2020-2022 秋水之冰 <27206617@qq.com>
+ * Copyright 2020 leo <2579186091@qq.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Ext;
+
+use Nervsys\LC\Factory;
+use Nervsys\Lib\App;
+
+class libLog extends Factory
+{
+    private string $log_path;
+    private string $log_file = 'default.log';
+
+    /**
+     * libLog constructor
+     *
+     * @param string $log_name
+     *
+     * @throws \ReflectionException
+     */
+    public function __construct(string $log_name = 'default')
+    {
+        $this->log_path = App::new()->log_path;
+        $this->log_file = date('Ymd') . '-' . $log_name . '.log';
+        unset($log_name);
+    }
+
+    /**
+     * Add logs
+     *
+     * @return $this
+     */
+    public function add(): self
+    {
+        $logs   = '';
+        $params = func_get_args();
+
+        foreach ($params as $param) {
+            $logs .= (is_array($param) || is_object($param))
+                ? json_encode($param, JSON_PRETTY)
+                : (string)$param;
+        }
+
+        $this->save($logs);
+
+        unset($logs, $params, $param);
+        return $this;
+    }
+
+    /**
+     * Save logs
+     */
+    private function save(string $logs): void
+    {
+        static $file = [];
+
+        $path = $this->log_path . DIRECTORY_SEPARATOR . $this->log_file;
+        $key  = hash('md5', $path);
+
+        if (!isset($file[$key])) {
+            $file[$key] = fopen($path, 'ab+');
+            chmod($path, 0666);
+        }
+
+        fwrite($file[$key], $logs . PHP_EOL . PHP_EOL);
+        unset($logs, $path, $key);
+    }
+}
