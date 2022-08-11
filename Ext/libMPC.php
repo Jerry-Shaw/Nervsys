ae12e69173ddfc71b9bef04e40173e22c0d15d88
Jerry <27206617@qq.com>
2022-08-11 20:36:35 +0800

    Change libMPC logic using ProcMgr

--- Ext/libMPC.php
+++ Ext/libMPC.php
@@ -22,331 +22,105 @@ namespace Nervsys\Ext;
 
 use Nervsys\Core\Factory;
 use Nervsys\Core\Lib\App;
-use Nervsys\Core\Lib\Caller;
 use Nervsys\Core\Lib\Error;
-use Nervsys\Core\Lib\IOData;
 use Nervsys\Core\Lib\Router;
-use Nervsys\Core\Mgr\OSMgr;
+use Nervsys\Core\Mgr\FiberMgr;
+use Nervsys\Core\Mgr\ProcMgr;
 
 class libMPC extends Factory
 {
-    public App    $app;
-    public Error  $error;
-    public Router $router;
-    public Caller $caller;
-    public IOData $IOData;
-    public OSMgr  $OSMgr;
-
-    public string $php_path;
-
-    public array $proc_idx   = [];
-    public array $proc_busy  = [];
-    public array $proc_list  = [];
-    public array $proc_await = [];
-
-    public array $pipe_read  = [];
-    public array $pipe_write = [];
+    public ProcMgr $procMgr;
 
     /**
-     * libMPC constructor
-     *
-     * @throws \ReflectionException
-     */
-    public function __construct()
-    {
-        $this->app    = App::new();
-        $this->error  = Error::new();
-        $this->caller = Caller::new();
-        $this->router = Router::new();
-        $this->IOData = IOData::new();
-        $this->OSMgr  = OSMgr::new();
-    }
-
-    /**
-     * Set PHP executable path
-     *
      * @param string $php_path
+     * @param int    $proc_num
      *
      * @return $this
+     * @throws \ReflectionException
+     * @throws \Throwable
      */
-    public function setPhpPath(string $php_path): self
+    public function create(string $php_path, int $proc_num = 10): self
     {
-        $this->php_path = &$php_path;
+        $app = App::new();
 
-        unset($php_path);
+        $MPC_command = $php_path . "\n"
+            . $app->script_path . "\n"
+            . '-c' . "\n" . '/' . __CLASS__ . '/childProc';
+
+        $this->procMgr = ProcMgr::new($MPC_command, $app->root_path);
+        $this->procMgr->create($proc_num);
+
+        unset($php_path, $proc_num, $MPC_command);
         return $this;
     }
 
     /**
-     * Start MPC
-     *
-     * @param int $max_fork
-     *
-     * @return $this
-     * @throws \Exception
-     */
-    public function start(int $max_fork = 10): self
-    {
-        $proc_cmd = $this->OSMgr
-            ->useProfile(true)
-            ->buildCmd('"' . $this->php_path . '" "' . $this->app->script_path . '" -c"/' . __CLASS__ . '/procUnit" -r"json"');
-
-        //Initialize processes and pipes
-        for ($i = 0; $i < $max_fork; ++$i) {
-            $proc = proc_open(
-                $proc_cmd,
-                [
-                    ['socket', 'rb'],
-                    ['socket', 'wb'],
-                    ['socket', 'wb']
-                ],
-                $pipes,
-                $this->app->root_path
-            );
-
-            if (!is_resource($proc)) {
-                throw new \Exception('Process initial error!', E_USER_ERROR);
-            }
-
-            stream_set_blocking($pipes[0], false);
-            stream_set_blocking($pipes[1], false);
-
-            $this->proc_idx[$i]  = 0;
-            $this->proc_busy[$i] = 0;
-
-            $this->pipe_write[$i] = $pipes[0];
-            $this->pipe_read[$i]  = $pipes[1];
-            $this->proc_list[$i]  = $proc;
-
-            fclose($pipes[2]);
-        }
-
-        //Register MPC closeAll function
-        register_shutdown_function([$this, 'closeAll']);
-
-        unset($max_fork, $proc_cmd, $i, $proc, $pipes);
-        return $this;
-    }
-
-    /**
-     * Add MPC job
-     *
      * @param string        $cmd
      * @param array         $data
      * @param callable|null $callable
      *
-     * @return void
+     * @return $this
+     * @throws \ReflectionException
+     * @throws \Throwable
      */
-    public function add(string $cmd, array $data = [], callable $callable = null): void
+    public function sendCMD(string $cmd, array $data, callable $callable = null): self
     {
         $data['c'] = &$cmd;
-        $free_proc = $this->getFreeProc();
-        $proc_idx  = array_pop($free_proc);
 
-        if (is_callable($callable)) {
-            $job_mid     = $proc_idx . dechex($this->proc_idx[$proc_idx]++);
-            $data['mid'] = &$job_mid;
+        $this->procMgr->sendArgv(json_encode($data), $callable);
 
-            $this->proc_await[$job_mid] = $callable;
-
-            unset($job_mid);
-        }
-
-        $this->proc_busy[$proc_idx] = 1;
-
-        fwrite($this->pipe_write[$proc_idx], json_encode($data, JSON_FORMAT) . "\n");
-
-        unset($cmd, $data, $callable, $free_proc, $proc_idx);
-    }
-
-    /**
-     * Await for all process done
-     *
-     * @return $this
-     */
-    public function await(): self
-    {
-        $write = $except = [];
-
-        while (in_array(1, $this->proc_busy, true)) {
-            $read = $this->pipe_read;
-
-            if (0 === (int)stream_select($read, $write, $except, 1)) {
-                continue;
-            }
-
-            $this->read($read);
-        }
-
-        unset($write, $except, $read);
+        unset($cmd, $data, $callable);
         return $this;
     }
 
     /**
-     * Close a process
-     *
-     * @param int $idx
+     * @return void
+     * @throws \ReflectionException
      */
-    public function closeProc(int $idx): void
+    public function childProc(): void
     {
-        if (is_resource($this->proc_list[$idx])) {
-            fclose($this->pipe_read[$idx]);
-            fclose($this->pipe_write[$idx]);
+        $error    = Error::new();
+        $router   = Router::new();
+        $fiberMgr = FiberMgr::new();
 
-            proc_close($this->proc_list[$idx]);
-        }
-
-        unset($this->proc_list[$idx], $idx);
-    }
-
-    /**
-     * Close All processes
-     */
-    public function closeAll(): void
-    {
-        foreach ($this->proc_list as $idx => $proc) {
-            $this->closeProc($idx);
-        }
-
-        unset($idx, $proc);
-    }
-
-    /**
-     * Daemon process unit
-     */
-    public function procUnit(): void
-    {
         while (true) {
-            //Pipe broken
             if (false === ($stdin = fgets(STDIN))) {
                 return;
             }
 
-            //Parse data
-            if ('' === $stdin || !is_array($data = json_decode($stdin, true))) {
+            $stdin = trim($stdin);
+
+            if ('' === $stdin) {
                 echo "\n";
                 continue;
             }
 
-            //Execute job
-            try {
-                $result = $this->execJob($data);
+            $data = json_decode($stdin, true);
 
-                if (isset($data['mid'])) {
-                    echo json_encode(['mid' => $data['mid'], 'data' => $result], JSON_FORMAT);
+            if (!is_array($data)) {
+                echo "\n";
+                continue;
+            }
+
+            try {
+                $cgi_cmd = $router->parseCgi($data['c']);
+
+                foreach ($cgi_cmd as $cmd_data) {
+                    $fiberMgr->async(
+                        $fiberMgr->await([parent::getObj($cmd_data[0], $data), $cmd_data[1]], $data),
+                        function (): string
+                        {
+                            echo json_encode(func_get_args(), JSON_FORMAT);
+                            echo "\n";
+                        }
+                    );
                 }
             } catch (\Throwable $throwable) {
+                $error->exceptionHandler($throwable, false, false);
                 unset($throwable);
             }
 
-            echo "\n";
-
-            unset($stdin, $data, $result);
+            unset($stdin, $data, $cgi_cmd, $cmd_data);
         }
-    }
-
-    /**
-     * Read pipe data & free process
-     *
-     * @param array $read
-     *
-     * @return void
-     */
-    private function read(array $read): void
-    {
-        foreach ($read as $idx => $pipe) {
-            while (!feof($pipe)) {
-                $msg = fgets($pipe);
-
-                if (false === $msg) {
-                    break;
-                }
-
-                $this->proc_busy[$idx] = 0;
-
-                if ('' === ($msg = trim($msg))) {
-                    continue;
-                }
-
-                $data = json_decode($msg, true);
-
-                if (is_array($data) && isset($data['mid']) && is_callable($this->proc_await[$data['mid']])) {
-                    call_user_func_array($this->proc_await[$data['mid']], array_values($data['data']));
-
-                    $this->proc_await[$data['mid']] = null;
-                    unset($this->proc_await[$data['mid']]);
-                }
-            }
-        }
-
-        unset($read, $idx, $pipe, $msg, $data);
-    }
-
-    /**
-     * Execute a job
-     *
-     * @param array $data
-     *
-     * @return array
-     * @throws \ReflectionException
-     * @throws \Throwable
-     */
-    private function execJob(array $data): array
-    {
-        $result = [];
-
-        try {
-            $cgi_cmd = $this->router->parseCgi($data['c']);
-
-            if (!empty($cgi_cmd)) {
-                while (is_array($cmd_data = array_shift($cgi_cmd))) {
-                    $result += $this->caller->runApiFn($cmd_data, $data);
-                }
-            }
-
-            $cli_cmd = $this->router->parseCli($data['c']);
-
-            if (!empty($cli_cmd)) {
-                while (is_array($cmd_data = array_shift($cli_cmd))) {
-                    $result += $this->caller->runProgram(
-                        $cmd_data,
-                        $data['argv'] ?? [],
-                        $data['cwd'] ?? '',
-                        $this->app->core_debug
-                    );
-                }
-            }
-        } catch (\Throwable $throwable) {
-            $this->error->exceptionHandler($throwable, false, false);
-        }
-
-        unset($data, $cgi_cmd, $cli_cmd, $cmd_data);
-        return $result;
-    }
-
-    /**
-     * Get free process idx list
-     *
-     * @return array
-     */
-    private function getFreeProc(): array
-    {
-        $free_proc = array_keys($this->proc_busy, 0, true);
-
-        if (!empty($free_proc)) {
-            return $free_proc;
-        }
-
-        $write = $except = [];
-        $read  = $this->pipe_read;
-
-        if (0 === (int)stream_select($read, $write, $except, 1)) {
-            return $this->getFreeProc();
-        }
-
-        $this->read($read);
-
-        unset($free_proc, $write, $except, $read);
-        return $this->getFreeProc();
     }
 }
