2dac10fa6d3258db5145758b9b1505c5c74f6c93
Jerry <27206617@qq.com>
2024-01-15 10:36:51 +0800

    Bump version to v8.2.1, issues fixed in ProcMgr.

--- NS.php
+++ NS.php
@@ -32,7 +32,7 @@ if (version_compare(PHP_VERSION, '8.1.0', '<')) {
 set_time_limit(0);
 ignore_user_abort(true);
 
-define('NS_VER', '8.2.0');
+define('NS_VER', '8.2.1');
 define('NS_NAME', 'Blueberry');
 define('NS_ROOT', __DIR__);
 define('NS_NAMESPACE', __NAMESPACE__);
